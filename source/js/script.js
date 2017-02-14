3b4920c048a0f946bf5324f6be3100ec7fa87c18
Haizs <Jmas2711@gmail.com>
2017-02-14 23:57:16 +0800

    Add nav for mobile.

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,13 @@
-$("header").headroom();
+$(function () {
+    $("header").headroom();
+});
+
+$("#nav-switch").on('click', function () {
+    $(this).toggleClass('header-switch-rotate');
+    return false;
+});
+
+$("#search-switch").on('click', function () {
+    $(this).toggleClass('header-switch-rotate');
+    return false;
+});
